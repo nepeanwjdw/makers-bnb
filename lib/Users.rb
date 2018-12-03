7f7d7ffc09be2bd5c5312fee247e96c56debf850
Will Wayman <will.jdw@gmail.com>
2018-12-03 17:31:53 +0000

    created persisted_users_data method
    Co-Authored-By: Paul Fazackerley <alittlecross@live.co.uk>

--- lib/Users.rb
+++ lib/Users.rb
@@ -3,10 +3,17 @@ require_relative 'DatabaseConnection'
 
 class User
 
-  attr_reader :id, :name, :email
+  attr_reader :user_id, :name, :email
+
+  def initialize(user_id:, name:, email:)
+    @user_id = user_id,
+    @name = name,
+    @email = email
+  end
 
   def self.create(name:, email:, password:)
     result = DatabaseConnection.query("INSERT INTO users (name, email, password) VALUES('#{name}', '#{email}', '#{password}') RETURNING user_id, name, email;")
+    User.new(user_id: result[0]['user_id'], name: result[0]['name'], email: result[0]['email'])
   end
 
 end
--- spec/Users_spec.rb
+++ spec/Users_spec.rb
@@ -1,16 +1,15 @@
 require 'Users'
+require 'database_helpers'
 
 describe User do
   describe '.create' do
     it 'creates a new user' do
-      User.create(name: 'Test User', email: 'test@user.com', password: 'password')
-      # persisted_users_data = persisted_users_data(id: user.id)
-      connection = PG.connect(dbname: "makers_bnb_test")
-      result = connection.query("SELECT * FROM users WHERE email = 'test@user.com';").first
-      # expect(user).to be_a User
-      # expect(result.user_id).to eq result['user_id']
-      expect(result['name']).to eq 'Test User'
-      expect(result['email']).to eq 'test@user.com'
+      user = User.create(name: 'Test User', email: 'test@user.com', password: 'password')
+      persisted_users_data = persisted_users_data(user_id: user.user.id)
+      expect(user).to be_a User
+      expect(user.user_id).to eq persisted_users_data['user_id']
+      expect(user.name).to eq 'Test User'
+      expect(user.email).to eq 'test@user.com'
     end
   end
 end
