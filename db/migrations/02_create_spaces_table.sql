05357590e534634b9bb1f6048ae79fe7fd728749
Stuart Adair <stu@silverka.co.uk>
2018-12-03 15:45:53 +0000

    Created Space class, can make new space

--- db/migrations/02_create_spaces_table.sql
+++ db/migrations/02_create_spaces_table.sql
@@ -1,1 +1,8 @@
-CREATE TABLE spaces(id SERIAL PRIMARY KEY, username VARCHAR(50), description VARCHAR(300), price INT);
+CREATE TABLE spaces(
+  space_id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  user_id INT,
+  description VARCHAR(300),
+  price INT,
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
