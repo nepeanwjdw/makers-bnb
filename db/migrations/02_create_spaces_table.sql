2025a19c68c331d272d27b726501d04fcda6f973
Will Wayman <will.jdw@gmail.com>
2018-12-03 14:34:54 +0000

    sql to create users table and update of spaces table
    Co-Authored-By: Paul Fazackerley <alittlecross@live.co.uk>

--- db/migrations/02_create_spaces_table.sql
+++ db/migrations/02_create_spaces_table.sql
@@ -1,1 +1,7 @@
-CREATE TABLE spaces(id SERIAL PRIMARY KEY, username VARCHAR(50), description VARCHAR(300), price INT);
+CREATE TABLE spaces(
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  description VARCHAR(300),
+  price INT,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
