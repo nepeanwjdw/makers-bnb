bb5782e1ae9f8208a71ae4a757a198ddccf0b157
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-12-04 11:34:43 +0000

    added datepicker

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'sinatra/flash'
 require './DatabaseConnection_setup'
 require_relative './lib/Users'
+require_relative './lib/Space'
 
 class MakersBNBApp < Sinatra::Base
   run! if app_file == $0
@@ -21,7 +22,7 @@ class MakersBNBApp < Sinatra::Base
     User.create(name: params[:name], email: params[:email], password: params[:password])
     redirect('/')
   end
-  
+
     get '/sign_in' do
     erb(:sign_in)
   end
@@ -30,8 +31,14 @@ class MakersBNBApp < Sinatra::Base
     User.fetch(email: params[:email], password: params[:password])
     redirect('/')
   end
-  
-  get '/create-space' do
+
+  get '/create_space' do
     erb(:create_space)
+  end
+
+  post '/create_space' do
+    # Space.create(name: params[:name], description: params[:description], price: params[:price], user_id: 1)
+    p params[:daterange]
+    redirect('/create_space')
   end
 end
