613a98508ba7d6eecb9ceb0368ff0d6756183662
Will Wayman <will.jdw@gmail.com>
2018-12-04 16:50:16 +0000

    fixed bug to pass show all spaces test
    Co-Authored-By: Paul Fazackerley <alittlecross@live.co.uk>

--- spec/Space_spec.rb
+++ spec/Space_spec.rb
@@ -33,12 +33,11 @@ describe Space do
       space_b = DatabaseConnection.query("INSERT INTO spaces (name, description, price, user_id) VALUES('The Ritz Flat', 'Snazzy!', 89.99, 15) RETURNING space_id, name, description, price, user_id")
       spaces = Space.all
 
-      expect(spaces.length).to eq 2
-      expect(spaces.first).to be_a Space
-      expect(spaces.first.space_id).to eq space_a[0]["space_id"].to_i
-      expect(spaces.first.user_id).to eq 15
-      expect(spaces.first.description).to eq 'Great!'
-      expect(spaces.first.price).to eq 49.99
+      expect(spaces.ntuples).to eq 2
+      expect(spaces[0]['space_id']).to eq space_a[0]["space_id"]
+      expect(spaces[0]['user_id']).to eq "15"
+      expect(spaces[0]['description']).to eq 'Great!'
+      expect(spaces[0]['price']).to eq "49.99"
     end
   end
 end
