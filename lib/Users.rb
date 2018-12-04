e1d8bfcd8d38847054cce9b4e1f884ff29c7404f
alittlecross <alittlecross@live.co.uk>
2018-12-04 11:02:31 +0000

    rubocop corrections

--- lib/Users.rb
+++ lib/Users.rb
@@ -1,8 +1,8 @@
 require 'pg'
 require_relative 'DatabaseConnection'
 
+# this class is class
 class User
-
   attr_reader :user_id, :name, :email
 
   def initialize(user_id:, name:, email:)
@@ -13,19 +13,27 @@ class User
 
   def self.create(name:, email:, password:)
     result = DatabaseConnection.query("
-      INSERT INTO users (name, email, password) 
-      VALUES('#{name}', '#{email}', '#{password}') 
+      INSERT INTO users (name, email, password)
+      VALUES('#{name}', '#{email}', '#{password}')
       RETURNING user_id, name, email;
       ")
-    User.new(user_id: result[0]['user_id'], name: result[0]['name'], email: result[0]['email'])
+    User.new(
+      user_id: result[0]['user_id'],
+      name: result[0]['name'],
+      email: result[0]['email']
+    )
   end
 
   def self.fetch(email:, password:)
     result = DatabaseConnection.query("
-      SELECT user_id, name, email 
-      FROM users 
+      SELECT user_id, name, email
+      FROM users
       WHERE email = '#{email}' AND password = '#{password}';
       ")
-    User.new(user_id: result[0]['user_id'], name: result[0]['name'], email: result[0]['email'])
+    User.new(
+      user_id: result[0]['user_id'],
+      name: result[0]['name'],
+      email: result[0]['email']
+    )
   end
 end
