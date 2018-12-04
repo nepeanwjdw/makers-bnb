4f448668be56fc28a67fd384dcef074c0987d502
Will Wayman <will.jdw@gmail.com>
2018-12-04 13:55:03 +0000

    wrote unit test for Space.all method
    Co-Authored-By: Paul Fazackerley <alittlecross@live.co.uk>

--- spec/Space_spec.rb
+++ spec/Space_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'database_helpers'
 require 'Space'
+require './lib/DatabaseConnection'
 
 describe Space do
 
@@ -25,4 +26,19 @@ describe Space do
     end
   end
 
+  describe '.all' do
+    it 'shows all spaces' do
+      DatabaseConnection.query("INSERT INTO users (user_id, name, email, password) VALUES(15, 'Test User', 'test@user.com', 'password');")
+      space_a = DatabaseConnection.query("INSERT INTO spaces (name, description, price, user_id) VALUES('Makers!', 'Great!', 49.99, 15) RETURNING space_id, name, description, price, user_id")
+      space_b = DatabaseConnection.query("INSERT INTO spaces (name, description, price, user_id) VALUES('The Ritz Flat', 'Snazzy!', 89.99, 15) RETURNING space_id, name, description, price, user_id")
+      spaces = Space.all
+
+      expect(spaces.length).to eq 2
+      expect(spaces.first).to be_a Space
+      expect(spaces.first.space_id).to eq space_a["space_id"]
+      expect(spaces.first.user_id).to eq 15
+      expect(spaces.first.description).to eq 'Great!'
+      expect(spaces.first.price).to eq 49.99
+    end
+  end
 end
