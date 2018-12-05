1d824c8fb612b8f782a62459d5b2b8b193d93878
Will Wayman <will.jdw@gmail.com>
2018-12-05 16:23:28 +0000

    updated view host spaces feature test

--- spec/features/host_dashboard_spec.rb
+++ spec/features/host_dashboard_spec.rb
@@ -1,19 +1,19 @@
 feature 'Host Dashboard' do
   background do
-    user = create_test_user
-    visit('/host_dashboard')
+    user = create_test_user_from_frontend
     Space.create(
       name: 'Makers!',
       description: 'Great!',
       price: 49.99,
-      user_id: user['user_id']
+      user_id: user.user_id
     )
     Space.create(
       name: 'The Ritz Flat',
       description: 'Snazzy!',
       price: 89.99,
-      user_id: user['user_id']
+      user_id: user.user_id
     )
+    visit('/host_dashboard')
   end
 
   xscenario 'hosts should be able to edit their Name' do
