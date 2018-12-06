eb2bc56cae7d1cc98748da2b8e10fe5cd512e412
Stuart Adair <stu@silverka.co.uk>
2018-12-06 16:48:21 +0000

    Password hashed using bcrypt

--- lib/Users.rb
+++ lib/Users.rb
@@ -1,4 +1,5 @@
 require_relative 'database_connection'
+require 'bcrypt'
 
 # top level comment
 class User
@@ -12,9 +13,10 @@ class User
 
   def self.create(name:, email:, password:)
     return nil if User.retrieve_by_email(email: email)
+    hashed_password = BCrypt::Password.create(password)
     result = DatabaseConnection.query("
       INSERT INTO users (name, email, password)
-      VALUES('#{name.gsub("'","''")}', '#{email.gsub("'","''")}', '#{password.gsub("'","''")}')
+      VALUES('#{name.gsub("'","''")}', '#{email.gsub("'","''")}', '#{hashed_password}')
       RETURNING user_id, name, email;
       ").first
     User.new(
@@ -43,6 +45,8 @@ class User
       SELECT * FROM users
       WHERE email = '#{email}';
       ").first
+      p result
+    return if result == nil
     User.new(
       user_id: result['user_id'],
       name: result['name'],
@@ -53,9 +57,10 @@ class User
   def self.authenticate(email:, password:)
     result = DatabaseConnection.query("
       SELECT * FROM users
-      WHERE email = '#{email.gsub("'","''")}' AND password = '#{password.gsub("'","''")}'
+      WHERE email = '#{email.gsub("'","''")}'
       ").first
     return if result.nil?
+    return unless BCrypt::Password.new(result['password']) == password
     User.new(
       user_id: result['user_id'],
       name: result['name'],
