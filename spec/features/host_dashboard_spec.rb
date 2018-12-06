bd0e46ea68ea1867313f5174ca95236de3e5f716
Will Wayman <will.jdw@gmail.com>
2018-12-06 12:34:29 +0000

    merged edit name and email feature tests into one test

--- spec/features/host_dashboard_spec.rb
+++ spec/features/host_dashboard_spec.rb
@@ -16,17 +16,13 @@ feature 'Host Dashboard' do
     visit('/host_dashboard')
   end
 
-  scenario 'hosts should be able to edit their Name' do
+  scenario 'hosts should be able to edit their Name and Email' do
     expect(page).to have_selector("input[value='Test User']")
-    fill_in('user_name', with: 'John')
-    click_on('Update Profile')
-    expect(page).to have_selector("input[value='John']")
-  end
-
-  scenario 'hosts should be able to edit their Email' do
     expect(page).to have_selector("input[value='test@email.com']")
+    fill_in('user_name', with: 'John')
     fill_in('user_email', with: 'john@email.com')
     click_on('Update Profile')
+    expect(page).to have_selector("input[value='John']")
     expect(page).to have_selector("input[value='john@email.com']")
   end
 
